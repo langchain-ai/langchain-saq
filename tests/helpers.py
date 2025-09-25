e2ac8fa06b32491820100776332812f469f16662
romain-priour-lc <romain@langchain.dev>
2025-09-24 17:45:11 -0700

    lint and tests passing + working on CI

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,10 +1,24 @@
 import typing as t
+from typing import cast
+
+from redis.asyncio.cluster import RedisCluster
+from redis.asyncio.client import Redis
 
 from saq.queue import Queue
 
 
 def create_queue(**kwargs: t.Any) -> Queue:
     return Queue.from_url("redis://localhost:6379", **kwargs)
+
+
+def create_cluster_queue(name: str = "default", **kwargs: t.Any) -> Queue:
+    redis = cast(Redis, RedisCluster.from_url("redis://localhost:30001"))
+    return Queue(
+        redis,
+        is_cluster=True,
+        name=name,
+        **kwargs,
+    )
 
 
 async def cleanup_queue(queue: Queue) -> None:
